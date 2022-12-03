977477fad775cd349e3202111d9ee01263fca7f5
Aivaria <mail.aivaria+github@gmail.com>
2022-12-03 06:47:45 +0100

    added day 3

---
+++ day3/rucksack.php
@@ -0,0 +1,51 @@
+<?php
+
+class rucksack
+{
+    protected $rucksacks;
+    protected $alphabet;
+
+    public function __construct()
+    {
+        $file = file_get_contents('inputfile.txt');
+        $this->rucksacks = explode(PHP_EOL, $file);
+        $this->alphabet = array_merge(range('a', 'z'), range('A', 'Z'));
+    }
+
+    public function getCompartmentsScore()
+    {
+        $score = 0;
+        foreach ($this->rucksacks as $rucksack) {
+            $compartments = $this->getCompartments($rucksack);
+            foreach (str_split($compartments[0]) as $letter) {
+                if (in_array($letter, str_split($compartments[1]))) {
+                    $score += array_search($letter, $this->alphabet) + 1;
+                    break;
+                }
+            }
+        }
+        return $score;
+    }
+
+    public function getGroupScore()
+    {
+        $score = 0;
+        $rucksacks = [];
+        foreach ($this->rucksacks as $masterrucksack) {
+            $rucksacks[] = $masterrucksack;
+            if (count($rucksacks) == 3) {
+                $result = array_intersect(str_split($rucksacks[0]), str_split($rucksacks[1]), str_split($rucksacks[2]));
+                $result = array_values($result)[0];
+                $score += (array_search($result, $this->alphabet) + 1);
+                $rucksacks = [];
+            }
+        }
+        return $score;
+    }
+
+    protected function getCompartments($line)
+    {
+        $result = str_split($line, strlen($line) / 2);
+        return $result;
+    }
+}
