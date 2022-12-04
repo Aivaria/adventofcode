1cd01216aff89ec1f71f71794c7c14a65f8617d0
Aivaria <mail.aivaria+github@gmail.com>
2022-12-04 19:37:20 +0100

    changes names

--- day4/index.php
+++ day4/index.php
@@ -6,7 +6,7 @@ $access = new accessCalc();
 <html>
 <head></head>
 <body>
-<h1>Advent of Code, Day 3, Elfen Rucksacks</h1>
+<h1>Advent of Code, Day 4, Elfen Access</h1>
 <br />
 Compartment Score: <?=$access->getDoubleAccess();?><br />
 Groups Score: <?=$access->getOverleapAccess();?><br />
